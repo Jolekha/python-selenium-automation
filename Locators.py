8fad3dda399cf7c20871202cc5bff7afacf1a920
jolekha <tinayunus@gmail.com>
2021-05-30 21:53:00 -0500

    amazon locators

---
+++ Locators.py
@@ -0,0 +1,12 @@
+"""
+amazon logo search by xpath  "i.a-icon.a-icon-logo"
+continue button search by ID  "Continue"
+need help line serch by Xpath  "span.a-expander-prompt"
+forget your password link search by ID "auth-fpp-link-bottom"
+other issue with sign in link search by ID "ap-other-signin-issues-link"
+create your amazon account button search by ID "createAccountSubmit"
+
+
+
+
+"""
